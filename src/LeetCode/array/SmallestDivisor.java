e74b278b888a1b25ed0f986e7a6a1eb7a7f86adc
manirdev <mhtnourmhtmjr@gmail.com>
2024-02-23 20:50:49 +0300

    Leetcode Binary Search

---
+++ src/LeetCode/array/SmallestDivisor.java
@@ -0,0 +1,34 @@
+package LeetCode.array;
+
+public class SmallestDivisor {
+    public static void main(String[] args) {
+        int[] nums = {1,2,5,9};
+        int threshold = 6;
+        int res = smallestDivisorHelper(nums, threshold);
+        System.out.println(res);
+    }
+
+    private static int smallestDivisorHelper(int[] nums, int threshold) {
+        int high = 0;
+        for (int i=0; i<nums.length; i++){
+            high = Math.max(high, nums[i]);
+        }
+        System.out.println(high);
+        int low = 1;
+        while (low <= high){
+            int mid = (low + high) / 2;
+            int sum  = 0;
+            for (int i=0; i<nums.length; i++){
+                sum = sum + (int)Math.ceil((double)nums[i]/mid);
+            }
+            if (sum > threshold){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+
+        }
+        return low;
+    }
+}
