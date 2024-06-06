d1f936e101abcc0db67adf7580a84fddb68b3aef
manirdev <mhtnourmhtmjr@gmail.com>
2024-06-06 23:33:45 +0300

    Leetcode Daily

--- src/LeetCode/strings/CommonChars.java
+++ src/LeetCode/strings/CommonChars.java
@@ -7,31 +7,37 @@ import java.util.Map;
 
 public class CommonChars {
     public static void main(String[] args) {
-        String[] words = {"bella", "label", "roller"};
+        String[] words = {"cool","lock","cook"};
         List<String> res = commonCharsHelper(words);
         System.out.println(res);
     }
 
     private static List<String> commonCharsHelper(String[] words) {
         List<String> res =  new ArrayList<>();
-        Map<Character, Integer> charFreq  = new HashMap<>();
-        Map<Character, Integer> freq  = new HashMap<>();
+        Map<Character, Integer> commonFreq  = new HashMap<>();
         for (char c : words[0].toCharArray()){
-            charFreq.put(c, charFreq.getOrDefault(c, 0) + 1);
+            commonFreq.put(c, commonFreq.getOrDefault(c, 0) + 1);
         }
         for (int i=1; i<words.length; i++) {
+            Map<Character, Integer> tempFreq  = new HashMap<>();
             for (char c : words[i].toCharArray()) {
-                freq.put(c, freq.getOrDefault(c, 0) + 1);
+                tempFreq.put(c, tempFreq.getOrDefault(c, 0) + 1);
             }
-        }
-        for (char key : charFreq.keySet()){
-            int value = charFreq.get(key);
-            if (freq.containsKey(key) && freq.get(key) == (value * (words.length - 1))){
-                int i = 0;
-                while (i < value){
-                    res.add(String.valueOf(key));
-                    i++;
+            Map<Character, Integer> minFreq  = new HashMap<>();
+            for (char key : commonFreq.keySet()) {
+                if (tempFreq.containsKey(key)) {
+                    int value = Math.min(commonFreq.get(key), tempFreq.get(key));
+                    minFreq.put(key, value);
                 }
+            }
+            commonFreq = minFreq;
+        }
+        for (char key : commonFreq.keySet()){
+            int value = commonFreq.get(key);
+            int i = 0;
+            while (i < value){
+                res.add(String.valueOf(key));
+                i++;
             }
         }
         return res;
